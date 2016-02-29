4eeaafc4063985e7b054380dcd2fcd80ce9ec260
benfri_m <mohamed.benfriha@epitech.eu>
2016-02-29 15:29:52 +0100

    add dir

---
+++ Linsolite/header.php
@@ -0,0 +1,75 @@
+<html>
+<head>
+    <title>Linsolite v1.0</title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+</head>
+<body>
+<div class="header">
+
+    <nav class="nav color-blue">
+        <div class="nav-wrapper mobile">
+
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="hide-on-med-and-down menu-c">
+                <li class="larg"><a href="sass.html"><i class="material-icons">home</i></a></li>
+                <li class="larg"><a class="dropdown-button" href="#!" data-activates="dropdown1"><i class="material-icons">view_list</i></a></li>
+                <li class="right blue-search">
+                    <form>
+                        <div class="input-field">
+                            <input id="search" type="search" required>
+                            <label for="search"><i class="material-icons">search</i></label>
+                            <i class="material-icons">close</i>
+                        </div>
+                    </form>
+                </li>
+
+                <ul id="dropdown1" class="dropdown-content category">
+                    <li class="larg"><a href="sass.html">Accueil</a></li>
+                    <li class="larg"><a href="badges.html">Actualité</a></li>
+                    <li class="larg"><a href="collapsible.html">Animaux</a></li>
+                    <li class="larg"><a href="mobile.html">Bagarre</a></li>
+                    <li class="larg"><a href="mobile.html">Choc</a></li>
+                    <li class="larg"><a href="mobile.html">Fail</a></li>
+                    <li class="larg"><a href="mobile.html">Humour</a></li>
+                    <li class="larg"><a href="mobile.html">Insolite</a></li>
+                    <li class="larg"><a href="mobile.html">Inclassable</a></li>
+                    <li class="larg"><a href="mobile.html">Technologie</a></li>
+                </ul>
+            </ul>
+
+
+            <ul class="side-nav color-blue" id="mobile-demo">
+                <li><a class="text-white" href="sass.html">Accueil</a></li>
+                <li class="text-white"><a href="badges.html">Actualité</a></li>
+                <li class="text-white"><a href="collapsible.html">Animaux</a></li>
+                <li class="text-white"><a href="mobile.html">Bagarre</a></li>
+                <li class="text-white"><a href="mobile.html">Choc</a></li>
+                <li class="text-white"><a href="mobile.html">Fail</a></li>
+                <li class="text-white"><a href="mobile.html">Humour</a></li>
+                <li class="text-white"><a href="mobile.html">Insolite</a></li>
+                <li class="text-white"><a href="mobile.html">Inclassable</a></li>
+                <li class="text-white"><a href="mobile.html">Technologie</a></li>
+                <li class="text-white"><a href="mobile.html">Nous-contacter</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="col s4 social">
+        <ul>
+            <li><img src="wp-content/themes/Linsolite/icon/dailymotion-logo.png" alt="dailymotion"></li>
+            <li><img src="wp-content/themes/Linsolite/icon/facebook-logo.png" alt="facebook"></li>
+            <li><img src="wp-content/themes/Linsolite/icon/twitter-social-logotype.png" alt="twitter"></li>
+            <li><img src="wp-content/themes/Linsolite/icon/instagram-social-network-logo-of-photo-camera.png" alt="instagram"></li>
+        </ul>
+    </div>
+
+    <div class="col s4 form">
+        <ul>
+            <li><img src="wp-content/themes/Linsolite/icon/new-email-envelope-back-symbol-in-circular-outlined-button.png" title="nous contacter" alt="contact"></li>
+            <li><img src="wp-content/themes/Linsolite/icon/upload-circular-button-variant.png" title="Envoyer une vidéo" alt="upload"></li>
+        </ul>
+    </div>
+
+    <img class="logo-center" src="http://linsolite.tv/wp-content/uploads/2015/01/trans.png" alt="linsolite">
+</div>
